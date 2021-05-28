307f9e3eec4df9942e057fe2798790a192cf12c2
AlexeyPot <potap2alex@gmail.com>
2021-05-28 17:18:58 +0300

    comp_table was changed

--- Tables/dbo.comp_table.sql
+++ Tables/dbo.comp_table.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[comp_table] (
   [id] [int] NOT NULL,
-  [comp1] AS ((1)+(1)),
+  [comp11] AS ((1)+(1)),
   [comp2] AS ((2)+(2)),
   [comp3] AS ((3)+(4)),
   PRIMARY KEY CLUSTERED ([id])
